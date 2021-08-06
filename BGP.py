208f4ed0b78a8028828ea3c79994116823ab4cb9
Freddy Morales <fredd99999@gmail.com>
2021-08-06 11:02:15 -0500

    First commit

---
+++ BGP.py
@@ -0,0 +1,16 @@
+from ncclient import manager
+from jinja2 import Template
+
+
+A = manager.connect(host='10.0.0.111', port=830, username='cisco',
+                   password='cisco', device_params={'name': 'csr'})
+
+bgp_template = Template(open('bgp.xml').read())
+bgp_rendered = bgp_template.render(BGP_AS=input("Please specify local AS: "),
+                                   NEIGHBOR_IP=input("enter neighbor IP address: "),
+                                   REMOTE_AS=input("Enter Remote AS Number: "),
+                                   UPDATE_INTERFACE=input("Enter Loopback ID: "))
+
+Configuration = A.edit_config(target='running', config=bgp_rendered)
+print(Configuration)
+
---
+++ Scenario1.py
@@ -0,0 +1,40 @@
+from ncclient import manager
+from lxml import etree
+
+
+def get_request(xmlstring):
+    print("XML FILTER:")
+    print(xmlstring)
+    print("-" * 80)
+    with manager.connect(host='10.0.0.111', port=830,
+                         username='cisco', password='cisco',
+                         hostkey_verify=False, device_params={},
+                         allow_agent=False, look_for_keys=False) as device:
+
+        netconf_get_reply = device.get(('subtree', xmlstring))
+
+    print("NETCONF RESPONSE:" )
+    print(etree.tostring(netconf_get_reply.data_ele, pretty_print=True).decode('utf-8'))
+    print("=" * 80)
+    print("=" * 80)
+    print("=" * 80)
+
+
+xml_filter = """ 
+    <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native"> 
+     <router> 
+     </router> 
+    </native> 
+"""
+
+print("NETCONF EXAMPLE 1: ")
+get_request(xml_filter)
+print("*" * 80)
+print("\n")
+
+
+
+
+
+
+
